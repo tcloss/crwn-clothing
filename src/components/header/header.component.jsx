fb1f34c0529eda25e4a7faec505a7c1bed9df2c0
tcloss@mac.com <tcloss@Thomass-MacBook-Pro.local>
2021-02-02 11:28:54 -0700

    added shopping cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {auth} from '../../firebase/firebase.utils';
 
-import {ReactComponent as Logo} from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
+import {ReactComponent as Logo} from '../../assets/crown.svg';
 
 import './header.styles.scss'
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
@@ -23,13 +25,16 @@ const Header = ({currentUser}) => (
                 :
                     <Link className='sign-on' to='/signin'>Sign in</Link>
             }
+            <CartIcon/>
         </div>
+        {hidden ? null : <CartDropdown/>}
     </div>
 
 )
             
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
